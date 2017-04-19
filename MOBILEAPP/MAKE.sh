ffa9d2ae97b8496e495e7c80a05f73ab4bc03530
Khoi Tran <kntran10@gmail.com>
2017-04-19 14:25:13 -0400

    Update MAKE.sh

--- MOBILEAPP/MAKE.sh
+++ MOBILEAPP/MAKE.sh
@@ -1,9 +1,9 @@
 npm install 
 npm rebuild node-sass
 ionic state reset
+ionic platform add android
+ionic platform add ios
 #For the navigator launcher
 npm install --save @ionic-native/launch-navigator
 #For the chart that gives you your breathing pulse when you connect a device
 npm install chart.js --save
-ionic platform add android
-ionic platform add ios

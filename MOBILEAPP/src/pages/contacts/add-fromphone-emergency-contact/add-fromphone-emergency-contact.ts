6d1a7f9d5890f22eb98eddde2166c964f1ed2844
Khoi Tran <knt36@drexel.edu>
2017-04-25 13:11:29 -0400

    Added tolerance to null strings and ordered while ignoring cases

--- MOBILEAPP/src/pages/contacts/add-fromphone-emergency-contact/add-fromphone-emergency-contact.ts
+++ MOBILEAPP/src/pages/contacts/add-fromphone-emergency-contact/add-fromphone-emergency-contact.ts
@@ -8,7 +8,35 @@ export class AddFromPhoneEmergencyContact {
 contacts: any;
     constructor(public deviceService: DeviceService) {
       deviceService.getAllPhoneContacts().then(list=>{
-        this.contacts = list.sort((c1, c2) => { return c1.displayName > c2.displayName });
+        this.contacts = list.sort((c1, c2) => {
+          var str1;
+          var str2;
+
+          if(!c1.displayName){
+            str1 = null;
+          }else{
+            str1 = c1.displayName.toUpperCase();
+          }
+
+          if(!c2.displayName){
+            str2 = null;
+          }else{
+            str2 = c2.displayName.toUpperCase();
+          }
+
+          if(str1 && !str2){
+            return(1);
+          }else if(str2 && !str1){
+            return(-1);
+          }
+           else if(str1 > str2){
+            return(1);
+          }else if(str2 > str1){
+            return(-1);
+          }else{
+            return(0);
+          }
+        });
       });
     }
 
